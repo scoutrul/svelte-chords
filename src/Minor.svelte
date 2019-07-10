fb739af89356c6cbfdc143f811ff68294c0aca68
Anton <scoutrul@mail.ru>
2019-07-10 22:45:19 +0300

    разукрасил ступени

--- src/Minor.svelte
+++ src/Minor.svelte
@@ -1,18 +1,6 @@
 <script>
     import emotion from 'emotion/dist/emotion.umd.min.js';
     const { css } = emotion;
-
-    const steps__cell = css`
-        display: flex;
-        align-content: center;
-        justify-content: center;
-        width: 80px;
-        background: #F4F4F4;
-    `;
-
-    const steps__cell_blank = css `
-        background: #EEEEEE;
-    `;
 	let state = {
         name: 'minor',
         steps: [
@@ -84,13 +72,41 @@
             }
         ]
     };
+    const steps__cell = css`
+        display: flex;
+        align-content: center;
+        justify-content: center;
+        width: 80px;
+        background: #F4F4F4;
+        outline: 1px solid #ccc;
+    `;
+    const step_I = `
+        background: #FCD;
+    `;
+
+    const steps__cell_blank = css `
+        background: #FFF;
+    `;
+
+    const stepStyles = {
+        I: css`background: #9dfffe`,
+        II: css`background: #fd9cfd`,
+        III: css`background: #cccdfd`,
+        IV: css`background: #fffe9f`,
+        V: css`background: #9eff9d`,
+        VI: css`background: #ff9d9e`,
+        VII: css`background: #EFEFEF`,
+    };
+
     $: findKey = (key, order) => {
         let harmony = ['A', 'H', 'C', 'D', 'E', 'F', 'G', 'A', 'H', 'C', 'D', 'E', 'F', 'G'];
         let indexOf = harmony.indexOf(key);
         return harmony[--order + indexOf];
     };
+    $: getStepStyles = (roman) => {
+        return stepStyles[roman];
+    };
     $: drowString = (key = 'A', startStep = 1) => {
-
         let steps = [...state.steps];
         let allHalfs = steps.reduce((acc, curr) => acc + curr.move.up, 0);
 
@@ -102,7 +118,7 @@
 
         let string = '';
         for (let step of steps) {
-            string += `<div class="${steps__cell}">${findKey(key, step.order)}</div>`;
+            string += `<div class="${steps__cell} ${getStepStyles(step.roman)}">${findKey(key, step.order)}</div>`;
             if(step.move.up === 2){
                 string += `<div class="${steps__cell} ${steps__cell_blank}"></div>`
             }
@@ -121,8 +137,8 @@
 
 {#each state.steps as step}
 <div class="alias">
-    <div class="alias__rom">{step.roman}</div>
-    <div>{step.name} / {step.type}</div>
+    <div class="{steps__cell} {getStepStyles(step.roman)}" >{step.roman}</div>
+    <div>&nbsp; {step.name} / {step.type}</div>
 </div>
 {/each}
 
@@ -154,7 +170,5 @@
 .alias {
     display: flex;
 }
-.alias__rom {
-    width: 40px;
-}
+
 </style>
