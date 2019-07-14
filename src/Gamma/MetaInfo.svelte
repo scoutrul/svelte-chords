41a9c5e940d8f642110d25f9409fb7081e1aa471
Anton <scoutrul@mail.ru>
2019-07-14 23:34:48 +0300

    чтото мелкое

--- src/Gamma/MetaInfo.svelte
+++ src/Gamma/MetaInfo.svelte
@@ -2,10 +2,12 @@
     <h1>{state.currentChord} {state.currentLad}</h1>
 
     {#each state.steps[state.currentLad] as step}
+        {#if step.roman}
         <div style="display: flex">
             <div class="{steps__cell} {getStepStyles(step.roman)}" >{step.roman}</div>
             <div>&nbsp; {step.name} / {step.type}</div>
         </div>
+        {/if}
     {/each}
 
 </div>
